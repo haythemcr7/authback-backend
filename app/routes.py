ae2f16570b18bd8d111d14e605d48c4aa94e0d9b
haythemcr7 <h.noam@elaraki.ac.ma>
2025-04-26 00:21:01 +0100

    fixroutes

--- app/routes.py
+++ app/routes.py
@@ -349,6 +349,7 @@ def passer_commande_anonyme():
     try:
         data = request.get_json()
         user_id = data.get("user_id")
+        username = data.get("username")
         table = data.get("table_numero")
         boissons = data.get("boissons")
         image_url = data.get("image_url", "")
@@ -360,6 +361,7 @@ def passer_commande_anonyme():
 
         commande = {
             "user_id": user_id,
+            "username":username,
             "table_numero": table,
             "boissons": boissons,
             "image_url": image_url,
